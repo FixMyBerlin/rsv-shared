cab2553a2ccff6e49e8a211a4ba4ea767bb66c87
Tobias <tobias@fixmycity.de>
2025-03-07 09:24:40 +0100

    Ensure push: Fix script

--- scripts/ensure-push.ts
+++ scripts/ensure-push.ts
@@ -3,7 +3,8 @@ import colors from 'colors'
 
 console.log(colors.inverse.green('[rsv-shared] ensure rsv-shared has no pending changes'))
 const folder = '../rsv-shared'
-const response = await $`git push --dry-run`.cwd(folder).text()
+const { stderr } = await $`git push --dry-run`.cwd(folder).quiet()
+const response = stderr.toString('utf-8')
 
 if (!response.includes('Everything up-to-date')) {
   console.log(
