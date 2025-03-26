7fcfa28b4f73f7c3aee6f5ce5e23e9faf525fa47
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-03-26 16:50:07 +0100

    Map: update margins in map components

--- sections/maps/SimpleMap.tsx
+++ sections/maps/SimpleMap.tsx
@@ -1,5 +1,4 @@
 import { COLORSCLASSES } from '@config/styles'
-import { Section } from '@shared/layouts/Section'
 import { featureCollection } from '@turf/turf'
 import type { InferEntrySchema } from 'astro:content'
 import clsx from 'clsx'
@@ -13,14 +12,12 @@ type Props = {
 
 export const SimpleMap = ({ features, label }: Props) => {
   return (
-    <Section className="mb-20">
-      <div className="relative h-[500px] w-full">
-        <BaseMap linePaint={linePaintSimpleMap} featureCollection={featureCollection(features)} />
-        <div className="flex w-full items-center gap-4 bg-gray-200 p-2 pl-6">
-          <div className={clsx('h-1 w-7', COLORSCLASSES.legendSimpleMap)} />
-          {label}
-        </div>
+    <div className="relative mb-20 h-[500px] w-full">
+      <BaseMap linePaint={linePaintSimpleMap} featureCollection={featureCollection(features)} />
+      <div className="flex w-full items-center gap-4 bg-gray-200 p-2 pl-6">
+        <div className={clsx('h-1 w-7', COLORSCLASSES.legendSimpleMap)} />
+        {label}
       </div>
-    </Section>
+    </div>
   )
 }
